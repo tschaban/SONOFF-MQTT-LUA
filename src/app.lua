9b4f8fb0a289cca7229eaf115d8244124a900cd8
adrian <dev@adrian.czabanowski.com>
2016-10-27 23:25:28 +0200

    template for main file

--- src/app.lua
+++ src/app.lua
@@ -1,5 +1,12 @@
 -- Application
 --
---
 -- LICENCE: http://opensource.org/licenses/MIT
 -- 2016-10-27 tschaban https://github.com/tschaban
+
+local module = {}
+
+
+function module.start()
+end
+
+return module
