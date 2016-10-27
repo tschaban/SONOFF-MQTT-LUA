7f6550a2a5a683f11928a4777d65e21de3fceb73
adrian <dev@adrian.czabanowski.com>
2016-10-27 23:24:30 +0200

    template for init file

--- src/init.lua
+++ src/init.lua
@@ -2,3 +2,9 @@
 --
 -- LICENCE: http://opensource.org/licenses/MIT
 -- 2016-10-27 tschaban https://github.com/tschaban
+
+app = require("app")
+config = require("config")
+setup = require("setup")
+
+setup.start()
